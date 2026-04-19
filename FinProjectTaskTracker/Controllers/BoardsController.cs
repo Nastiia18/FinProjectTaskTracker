6ef16d31f32820815e441889c422aec296bdb5e5
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2026-04-19 16:25:03 +0300

    feat: add boards API and core models

---
+++ FinProjectTaskTracker/Controllers/BoardsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinProjectTaskTracker.Models;
+
+
+namespace FinProjectTaskTracker.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class BoardsController : ControllerBase
+{
+    private static List<Board> boards = new List<Board>();
+
+
+    [HttpGet]
+    public IActionResult GetBoards()
+    {
+        return Ok(boards);
+    }
+
+
+    [HttpPost]
+    public IActionResult CreateBoard(Board board)
+    {
+        if (boards.Any(b => b.Name == board.Name))
+            return BadRequest("Board name must be unique");
+
+
+        board.Id = Guid.NewGuid();
+        board.CreatedAt = DateTime.UtcNow;
+
+
+        boards.Add(board);
+
+
+        return Ok(board);
+    }
+}
